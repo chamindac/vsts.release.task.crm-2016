5c524446d0e61003cc0b9a2e01b82091636efc5d
Chaminda Chandrasekara <chamindac@navantis.com>
2016-09-30 16:42:19 +0530

    CRM Solution Import and Activate Workflow tasks for VSTS and TFS

---
+++ CRM-2016/CRM2016/activate-crm-workflows/ActivateCRMWorkflows.ps1
@@ -0,0 +1,128 @@
+﻿param(
+    [Parameter(mandatory=$true)]
+    [string]$UserName,
+    [Parameter(mandatory=$true)]
+    [string]$UserPwd,
+    [Parameter(mandatory=$true)]
+    [string]$CRMSvrUrl,
+    [Parameter(mandatory=$true)]
+    [string]$CRMOrgName,
+    [string[]]$wfIdsToDeactivate    
+   )
+
+$ErrorActionPreference = "Stop"
+
+Write-Host "Version 1.1.5"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+
+Import-Module $PSScriptRoot\PSModules\Microsoft.Xrm.Data.PowerShell -Verbose:$false -WarningAction SilentlyContinue
+
+Write-Host "Connecting to $CRMSvrUrl $CRMOrgName ... "
+
+$secpasswd = ConvertTo-SecureString $UserPwd -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($UserName, $secpasswd)
+
+$crmcon = Get-CrmConnection -ServerUrl $CRMSvrUrl -OrganizationName $CRMOrgName -Credential $cred  -Verbose #-Timeout $CRMTimeout
+
+Write-Host "CRM connection obtained."
+
+$workfows = Get-CrmRecords -EntityLogicalName "workflow" -conn $crmcon -Fields "workflowid","name","statecode","statuscode" -WarningAction SilentlyContinue
+
+Write-Host ("Total workflows found:{0}" -f $workfows.CrmRecords.count)
+
+$activateStatecodeStatus = "Activated"
+$activateStatuscodeStatusReason = "Activated"
+$deactivateStatecodeStatus = "Draft"
+$deactivateStatuscodeStatusReason = "Draft"
+
+$ActivatedCount=0
+$DeactivatedCount=0
+$ActivationFailsCount=0
+$DeactivationFailsCount=0
+$ActivationSkipCount=0
+$DeactivationSkipCount=0
+$ActivationUnknownCount=0
+$DeactivationUnknownCount=0
+
+foreach($workflow in  $workfows.CrmRecords)
+{
+    if (($wfIdsToDeactivate -ne $null) -and $wfIdsToDeactivate.Contains($workflow.workflowid.Guid))
+    {
+        # Deactivating workflow since set to deativate
+        if ($workflow.statecode -eq $activateStatecodeStatus)
+        {   try{ 
+                Write-Host ("Deactivating... workflow with id:{0} have statecode:{1} statuscode:{2}, name:{3}" -f $workflow.workflowid, $workflow.statecode, $workflow.statuscode, $workflow.name)
+                Set-CrmRecordState -conn $crmcon -CrmRecord $workflow -StateCode $deactivateStatecodeStatus -StatusCode $deactivateStatuscodeStatusReason -Verbose
+                Write-Warning ("##vso[task.logissue type=warning;]Deactivated workflow with id:{0}, name:{1}" -f $workflow.workflowid, $workflow.name)
+                $DeactivatedCount=$DeactivatedCount+1
+            }
+            catch{
+                Write-Warning ("##vso[task.logissue type=warning;]{0}" -f $crmcon.LastCrmError) 
+                $DeactivationFailsCount=$DeactivationFailsCount+1
+            }
+        }
+        elseif($workflow.statecode -eq $deactivateStatecodeStatus)
+        {
+            Write-Host ("Already deactivated - workflow with id:{0}, name:{1}" -f $workflow.workflowid, $workflow.name)
+            $DeactivationSkipCount=$DeactivationSkipCount+1
+        }
+        else
+        {
+            Write-Warning ("##vso[task.logissue type=warning;]Workflow with id:{0} have statecode:{1} statuscode:{2}, name:{3}" -f $workflow.workflowid, $workflow.statecode, $workflow.statuscode, $workflow.name)
+            $DeactivationUnknownCount=$DeactivationUnknownCount+1
+        }
+    }
+    elseif($workflow.statecode -eq $activateStatecodeStatus)
+    {
+        Write-Host ("Already activated - workflow with id:{0}, name:{1}" -f $workflow.workflowid, $workflow.name)
+        $ActivationSkipCount=$ActivationSkipCount+1
+    }
+    elseif($workflow.statecode -eq $deactivateStatecodeStatus)
+    {
+        # 0,1 to 1,2 (state,status) to activate
+        
+        try{
+            Write-Host ("Activating... workflow with id:{0} have statecode:{1} statuscode:{2}, name:{3}" -f $workflow.workflowid, $workflow.statecode, $workflow.statuscode, $workflow.name)
+            Set-CrmRecordState -conn $crmcon -CrmRecord $workflow -StateCode $activateStatecodeStatus -StatusCode $activateStatuscodeStatusReason -Verbose
+            Write-Host ("Activated workflow with id:{0}, name:{1}" -f $workflow.workflowid, $workflow.name)
+            $ActivatedCount=$ActivatedCount+1
+        }
+        catch{
+            Write-Warning ("##vso[task.logissue type=warning;]{0}" -f $crmcon.LastCrmError)
+            $ActivationFailsCount=$ActivationFailsCount+1 
+        }
+    }
+    else{
+        Write-Warning ("##vso[task.logissue type=warning;]Workflow with id:{0} have statecode:{1} statuscode:{2}, name:{3}" -f $workflow.workflowid, $workflow.statecode, $workflow.statuscode, $workflow.name)
+        $ActivationUnknownCount=$ActivationUnknownCount+1
+    }    
+}
+
+Write-Host "-------------------------------------------------------"
+Write-Host ("Total workflows processed:{0}" -f $workfows.CrmRecords.count)
+Write-Host "-------------------------------------------------------"
+Write-Host ("Activated: {0}, Already Activated:{1}, Total Activated:{2}" -f $ActivatedCount, $ActivationSkipCount, ($ActivatedCount + $ActivationSkipCount))
+if($wfIdsToDeactivate -ne $null)
+{
+    Write-Host ("Deactivated: {0}, Already Deactivated:{1}, Total Deactivated:{2}" -f $DeactivatedCount, $DeactivationSkipCount, ($DeactivatedCount + $DeactivationSkipCount))
+}
+if($ActivationFailsCount -gt 0)
+{
+    Write-Warning ("##vso[task.logissue type=warning;]Activation fails for {0} workflows. check detailed log for reasons." -f $ActivationFailsCount)
+}
+if($DeactivationFailsCount -gt 0)
+{
+    Write-Warning ("##vso[task.logissue type=warning;]Deactivation fails for {0} workflows. check detailed log for reasons." -f $DeactivationFailsCount)
+}
+if($ActivationUnknownCount -gt 0)
+{
+    Write-Warning ("##vso[task.logissue type=warning;]Activation unknown status for {0} workflows. check detailed log for reasons." -f $ActivationUnknownCount)
+}
+if($DeactivationUnknownCount -gt 0)
+{
+    Write-Warning ("##vso[task.logissue type=warning;]Deactivation unknown status for {0} workflows. check detailed log for reasons." -f $DeactivationUnknownCount)
+}
+Write-Host "-------------------------------------------------------"
+
+Remove-Module Microsoft.Xrm.Data.PowerShell -Verbose:$false
---
+++ CRM-2016/CRM2016/import-crm-solution/ImportCRMSolution.ps1
@@ -0,0 +1,79 @@
+﻿#
+# ImportCRMSolution.ps1
+#
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param(
+    [Parameter(mandatory=$true)]
+    [string]$UserName,
+    [Parameter(mandatory=$true)]
+    [string]$UserPwd,
+    [Parameter(mandatory=$true)]
+    [string]$CRMSvrUrl,
+    [Parameter(mandatory=$true)]
+    [string]$CRMOrgName,
+    [Parameter(mandatory=$true)]
+    [string]$CRMSolutionName,
+    [Parameter(mandatory=$true)]
+    [string]$SolutionZipFilePath,
+    [Parameter(mandatory=$true)]
+    [string] $CRMSolutionManaged,
+    [Parameter(mandatory=$true)]
+    [int]$SolutionImportWaitSeconds
+   )
+
+$ErrorActionPreference = "Stop"
+Write-Host "Version 1.1.5"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+
+Import-Module $PSScriptRoot\PSModules\Microsoft.Xrm.Data.PowerShell -Verbose:$false -WarningAction SilentlyContinue
+
+Write-Host "Connecting to $CRMSvrUrl $CRMOrgName ... "
+
+$secpasswd = ConvertTo-SecureString $UserPwd -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($UserName, $secpasswd)
+
+$crmcon = Get-CrmConnection -ServerUrl $CRMSvrUrl -OrganizationName $CRMOrgName -Credential $cred  -Verbose #-Timeout $CRMTimeout
+
+Write-Host "CRM connection obtained."
+
+
+
+$SolutionZipFilePath = Resolve-Path $SolutionZipFilePath 
+if($CRMSolutionManaged.ToLower() -eq "true")
+{
+    Write-Host ("Importing Managed CRM Solution: {0} with file: {1} ..." -f $CRMSolutionName, $SolutionZipFilePath) 
+}
+else
+{
+    Write-Host ("Importing Unmanaged CRM Solution: {0} with file: {1} ..." -f $CRMSolutionName, $SolutionZipFilePath) 
+}
+
+try{
+	#Try importing with override customizations    
+	Import-CrmSolution -conn $crmcon -SolutionFilePath $SolutionZipFilePath -MaxWaitTimeInSeconds $SolutionImportWaitSeconds -OverwriteUnManagedCustomizations -ActivatePlugIns -PublishChanges -Verbose
+	Write-Host ("Importing completed for CRM solution: {0} with file: {1}." -f $CRMSolutionName, $SolutionZipFilePath)	
+
+				
+
+}Catch {
+
+    if($CRMSolutionManaged.ToLower() -eq "true") # only try hold import if this is a managed solution
+    {
+		Write-Warning ("##vso[task.logissue type=warning;]Importing failed for CRM Solution: {0} with file: {1}." -f $CRMSolutionName, $SolutionZipFilePath)
+        Write-Warning ("##vso[task.logissue type=warning;]{0}" -f $_.Exception)
+								
+		Write-Host "--------------------------------------------------------------------------------------------"
+		Write-Warning ("##vso[task.logissue type=warning;]Trying import with hold promote mechanism for CRM Solution: {0} with file {1}." -f $CRMSolutionName, $SolutionZipFilePath)
+		Import-CrmSolution -conn $crmcon -SolutionFilePath $SolutionZipFilePath -MaxWaitTimeInSeconds $SolutionImportWaitSeconds -ImportAsHoldingSolution -ActivatePlugIns -PublishChanges -Verbose
+		Write-Host ("Importing with hold completed for CRM Solution: {0} with file {1}." -f $CRMSolutionName, $SolutionZipFilePath)	
+		Write-Host "--------------------------------------------------------------------------------------------"
+				
+		Write-Host ("Promoting CRM Solution {0} ." -f $CRMSolutionName)	
+		DeleteAndPromote-CrmSolution -conn $crmcon -SolutionName $CRMSolutionName -MaxWaitTimeInSeconds $SolutionImportWaitSeconds -Verbose
+		Write-Host ("Promoting CRM Solution {0} ." -f $CRMSolutionName)	
+
+		}
+}
+
+Remove-Module Microsoft.Xrm.Data.PowerShell -Verbose:$false
